39cc6756e1c2f5782c0729f99f856f371ff681ea
muaz64 <muazmuhammad21@gmail.com>
2025-01-26 20:31:04 +0600

    app.jsx update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from "react";
-import ProjectCard from "./components/ProjectsSection";
-import "./App.css"
+import HeroSection from "./components/HeroSection";
+import ProjectSection from "./components/ProjectsSection";
+import BackToTopButton from "./components/BackToTopButton";
 
-
-
+const App = () => {
   return (
-    <div className="min-h-screen bg-gray-500 flex flex-col items-center p-6">
-      <h1 className="text-4xl font-bold text-blue-600 mb-6">My Mini JS Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            image={project.image}
-            title={project.title}
-            description={project.description}
-            liveLink={project.liveLink}
-          />
-        ))}
-      </div>
+    <div className="w-full">
+      <HeroSection />
+      <ProjectSection />
+      <BackToTopButton />
     </div>
   );
 };
